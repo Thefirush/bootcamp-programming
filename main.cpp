5e2cca7e0c3b5a93860e9d589f6e4815e3b397ae
Thefirush <zefirush@icloud.com>
2021-08-04 09:16:10 +0300

    task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-
+#include <cmath>
+using namespace std;
+bool prime(int a) {
+    for(int i = 2; i <= a - 1; i++)
+        if(a%i==0)
+            return 0;
+        return 1;
+}
 int main() {
-    std::cout << "!Hello, World!21" << std::endl;
+    for(int i = 1; i <= 100; i++) {
+        cout << i << " " << prime(i) << " " << prime(pow(2, i)-1) << endl;
+        if (prime(i)!=prime(pow(2,i)-1)){
+            return 0;
+        }
+    }
     return 0;
 }
