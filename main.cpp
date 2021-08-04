7820e535cc49041b0925e847690fc4a664e6b7bf
Thefirush <zefirush@icloud.com>
2021-08-04 09:17:31 +0300

    task hw day 3

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
+
 bool prime(int a) {
-    for(int i = 2; i <= a - 1; i++)
-        if(a%i==0)
+    for (int i = 2; i <= a - 1; i++)
+        if (a % i == 0)
             return 0;
-        return 1;
+    return 1;
 }
+
 int main() {
-    for(int i = 1; i <= 100; i++) {
-        cout << i << " " << prime(i) << " " << prime(pow(2, i)-1) << endl;
-        if (prime(i)!=prime(pow(2,i)-1)){
+    for (int i = 1; i <= 100; i++) {
+        cout << i << " " << prime(i) << " " << prime(pow(2, i) - 1) << endl;
+        if (prime(i) != prime(pow(2, i) - 1)) {
             return 0;
         }
     }
